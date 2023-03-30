0f14da65437aff5d34230ac5afdca976cb9b8e8d
Ethan Albers <ekalbers22@gmail.com>
2023-03-29 22:59:40 -0500

    finished lab 08

---
+++ README_LAB09.md
@@ -0,0 +1,21 @@
+# Lab 08 - ten-thousand
+## Author: Ethan Albers and Dominick Martin
+
+### Links and Resources
+- Obtained tests from class repository 
+- [ten-thousand repository](https://github.com/ekalbers/ten-thousand.git)
+
+### How to initialize/run your application
+~~~
+python3 -m env .venv
+source .venv/bin/activate
+pip install pytest
+~~~
+#### run:`python3 ten_thousand/dice.py`
+
+### How do you run tests?
+
+#### tests:`pytest -m version_3`
+
+### Notes:
+- All test for this labs and previous labs now passing including simulations.
--- ten_thousand/dice.py
+++ ten_thousand/dice.py
@@ -1,12 +1,21 @@
-from game_logic import GameLogic
+from ten_thousand.game_logic import GameLogic
+
+dice_roller = GameLogic.roll_dice
+
+
+def play(roller):
+    global dice_roller
+    dice_roller = roller
+    global game
+    game = GameLogic()
+    intro()
 
 
 def intro():
-    global options
-    print('welcome to Ten Thousand')
+    print('Welcome to Ten Thousand')
     print('(y)es to play or (n)o to decline')
     choice = input('> ')
-    options.get(choice, lambda: print('invalid input'))()
+    options.get(choice.strip(), lambda: print('invalid input'))()
 
 
 def decline():
@@ -21,19 +30,37 @@ def start_round():
 
 def roll_dice():
     print(f'Rolling {game.dice_left} dice...')
-    dice = game.roll_dice()
+    dice = dice_roller(game.dice_left)
     dice_string = '*** '
     for x in dice:
         dice_string = dice_string + str(x) + ' '
     dice_string += '***'
     print(dice_string)
-    print('Enter dice to keep, or (q)uit:')
-    choice = input('> ')
-    if choice == 'q':
-        quit_game()
-    else:
-        score = GameLogic.calculate_score(game.set_aside(choice, dice))
+    if GameLogic.calculate_score(dice) == 0:
+        print('****************************************')
+        print('**        Zilch!!! Round over         **')
+        print('****************************************')
+        print(f'You banked 0 points in round {game.round}')
+        print(f'Total score is {game.total_points} points')
+        start_round()
+        return
+    format_choice = False
+    choice = ' '
+    while not format_choice and choice != 'q':
+        print('Enter dice to keep, or (q)uit:')
+        choice = input('> ')
+        if choice == 'q':
+            quit_game()
+            format_choice = True
+        else:
+            format_choice = check_cheating(dice, choice)
+            if not format_choice:
+                print(dice_string)
+    if choice != 'q':
+        score = GameLogic.calculate_score(game.set_aside(format_choice.strip(), dice))
         game.set_current_points(score)
+        if len(game.set_aside_dice) == 6:
+            game.hot_dice()
         print(f'You have {game.current_points} unbanked points and {game.dice_left} dice remaining')
         print('(r)oll again, (b)ank your points or (q)uit:')
         choice = input('> ')
@@ -49,6 +76,25 @@ def bank():
 
 def quit_game():
     print(f'Thanks for playing. You earned {game.total_points} points')
+
+
+def check_cheating(dice, choice):
+    dice_numbers = ['1', '2', '3', '4', '5', '6']
+    other_numbers = ['0', '7', '8', '9']
+    number_string = ''
+    dice_list = list(dice)
+    for x in choice:
+        if x in dice_numbers:
+            if int(x) not in dice_list:
+                print(f'Cheater!!! Or possibly made a typo...')
+                return False
+            else:
+                dice_list.remove(int(x))
+                number_string += f'{x} '
+        elif x in other_numbers:
+            print(f'{x} is not a valid number')
+            return False
+    return number_string
 
 
 options = {
